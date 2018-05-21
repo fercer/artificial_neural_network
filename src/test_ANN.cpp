2f246c9dcaabc080fffda96ce0f9f7a27bbeb070
fercer <iie.fercer@gmail.com>
2018-05-21 16:43:05 -0500

    Levenberg marquardt tested using parallel computing and the iris dataset, everything works fine ...

--- src/test_ANN.cpp
+++ src/test_ANN.cpp
@@ -9,27 +9,29 @@ int main(int argc, char * argv[])
 {
 	backpropagationBasedANN bpt_ann_test;
 	//bpt_ann_test.loadNetworkData("XOR_trained_network.xml");
-	bpt_ann_test.loadNetworkData("Trained_network_test.xml");
+	//bpt_ann_test.loadNetworkData("Trained_network_test.xml");
+	bpt_ann_test.loadNetworkData("iris_network.xml");
 
-	data_handler testing_data;
-	testing_data.setFilename("trainingDataXOR_slim.txt");
-	testing_data.loadData();
+	data_handler training_data;
+	//training_data.setFilename("trainingDataXOR_slim.txt");
+	training_data.setFilename("training_set.dat");
+	training_data.loadData();
 
-	const unsigned int testing_size = testing_data.getDataSize();
-	const unsigned int inputs_count = testing_data.getInputSize();
-	const unsigned int outputs_count = testing_data.getOutputSize();
+	const unsigned int training_size = training_data.getDataSize();
+	const unsigned int inputs_count = training_data.getInputSize();
+	const unsigned int outputs_count = training_data.getOutputSize();
 
-	double **testing_patterns_database = testing_data.getInputData();
-	int **testing_outputs_database = testing_data.getOutputData();
+	double **training_patterns_database = training_data.getInputData();
+	int **training_outputs_database = training_data.getOutputData();
 	
 	bpt_ann_test.setBackpropagationMethod(backpropagationBasedANN::BPM_LEVENBERG_MARQUARDT);
-	bpt_ann_test.setEpochs(1000);
+	bpt_ann_test.setEpochs(1795);
 	bpt_ann_test.setLearningRates(0.05);
 	bpt_ann_test.setMomentums(0.9);
 	bpt_ann_test.setTargetLoss(0.01);
-	bpt_ann_test.setTrainingDataSize(testing_size);
-	bpt_ann_test.setTrainingData(testing_patterns_database);
-	bpt_ann_test.setGroundtruthData(testing_outputs_database);
+	bpt_ann_test.setTrainingDataSize(training_size);
+	bpt_ann_test.setTrainingData(training_patterns_database);
+	bpt_ann_test.setGroundtruthData(training_outputs_database);
 	bpt_ann_test.setNetworkLogFilename("Trained_network_test.xml");
 
 	for (unsigned int output_index = 0; output_index < outputs_count; output_index++)
@@ -45,9 +47,19 @@ int main(int argc, char * argv[])
 	trained_ann.setNetworkLogFilename("testing_network_architecture.xml");
 	trained_ann.saveNetworkState();
 
+
+	data_handler testing_data;
+	testing_data.setFilename("testing_set.dat");
+	testing_data.loadData();
+
+	const unsigned int testing_size = testing_data.getDataSize();
+
 	double * prediction = (double*)malloc(outputs_count * sizeof(double));
-	
-	for (unsigned int pattern_index = 0; pattern_index < (unsigned int)(0.1*(double)testing_size); pattern_index++)
+
+	double **testing_patterns_database = testing_data.getInputData();
+	int **testing_outputs_database = testing_data.getOutputData();
+
+	for (unsigned int pattern_index = 0; pattern_index < testing_size; pattern_index++)
 	{
 		double *testing_pattern = *(testing_patterns_database + pattern_index);
 		int *ground_truth = *(testing_outputs_database + pattern_index);
